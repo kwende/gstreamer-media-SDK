8d4995a1f4f12f1a295a25256bce0abefa61351a
Ben <ben@ben-rush.net>
2019-05-07 16:15:09 -0500

    Fork for my laptop.

---
+++ gst-mfx-build_msvc_laptop/version.h
@@ -0,0 +1,14 @@
+/* Name of package */
+#define PACKAGE "gst_mfx"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "GStreamer MFX"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "GStreamer MFX 2.0.2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "gst-mfx"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.0.2"
